48228655bd45db1b2b86d65ea15f754fa3932884
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-11-04 16:19:23 +0000

    updated main.py.

--- main.py
+++ main.py
@@ -32,8 +32,40 @@ try:
 except ResponseError as error:
     print(error)
 
-
 flight_data = []
+
+
+def construct_flight_data(flight_obj, index):
+    obj = {}
+    temp = []
+    flight_obj_size = len(flight_obj)
+    for j, data in enumerate(flight_obj, 1):
+        size = len(data["segments"])
+        for num in range(0, size):
+            temp.append(
+                {
+                    "Trip"
+                    if j == 1
+                    else "Return Trip": {
+                        "Fight"
+                        if size == 1
+                        else "First fight"
+                        if num == 0
+                        else "Second fight": [
+                            data["segments"][num]["departure"]["iataCode"],
+                            data["segments"][num]["arrival"]["iataCode"],
+                        ],
+                        "Departure time": data["segments"][num]["departure"]["at"],
+                        "Arrival time": data["segments"][num]["arrival"]["at"],
+                        "Duration": data["segments"][num]["duration"],
+                    }
+                }
+            )
+    obj[index] = temp
+    flight_data.append(obj)
+    print(obj)
+
+
 try:
     response = amadeus.shopping.flight_offers_search.get(
         originLocationCode="BOM",
@@ -45,28 +77,11 @@ try:
         currencyCode="INR",
         max=5,
     )
-    for num, x in enumerate(response.data, 1):
-        print(num)
+
+    for index, x in enumerate(response.data, 1):
+        print(index)
         print(x["price"]["total"])
-        
-        size = len(x["itineraries"][0]["segments"])
-        for num in range(0, size):
-            obj = {
-                "Fight"
-                if size == 1
-                else "First fight"
-                if num == 0
-                else "Second fight": [
-                    x["itineraries"][0]["segments"][num]["departure"]["iataCode"],
-                    x["itineraries"][0]["segments"][num]["arrival"]["iataCode"],
-                ],
-                "Departure time": x["itineraries"][0]["segments"][num]["departure"][
-                    "at"
-                ],
-                "Arrival time": x["itineraries"][0]["segments"][num]["arrival"]["at"],
-                "Duration": x["itineraries"][0]["segments"][num]["duration"],
-            }
-            print(obj)
+        construct_flight_data(x["itineraries"], index)
         print()
 
 except ResponseError as error:
