b6d503a8e0a9c9757c07b57f52bdfb2eae5476f3
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-11-05 12:34:05 +0000

    Storing token to local storage and implemenedt external API call in front-end.

--- FPP_client/src/App.jsx
+++ FPP_client/src/App.jsx
@@ -1,9 +1,89 @@
-import { printData } from './amadeus_api.js';
+import axios from "axios";
 import './App.css';
+import { useState, useEffect } from "react";
 
 
 function App() {
-  printData();
+
+  const [data, setData] = useState(null);
+
+  const apiKey = process.env.API_KEY;
+  const apiSecret = process.env.API_SECRET;
+
+  const accessT = localStorage.getItem("access_token");
+  const expiresIn = localStorage.getItem("expires_in");
+
+  const searchParams = {
+    originLocationCode: "DEL",
+    destinationLocationCode: "BOM",
+    departureDate: "2023-12-24",
+    returnDate: "2023-12-25",
+    adults: 1,
+    currencyCode: "INR",
+    max: 5,
+  };
+
+  const currentTimeInSeconds = () => { return Math.floor(Date.now() / 1000); };
+
+
+  const fetchAcessToken = async () => {
+
+    const tokenRequestData = {
+      grant_type: "client_credentials",
+      client_id: apiKey,
+      client_secret: apiSecret,
+    };
+
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+
+    try {
+      const response = await axios
+        .post(
+          "https://test.api.amadeus.com/v1/security/oauth2/token",
+          tokenRequestData,
+          {
+            headers: headers,
+          }
+        );
+      localStorage.setItem("access_token", response.data.access_token);
+      localStorage.setItem("expires_in", currentTimeInSeconds() + response.data.expires_in);
+    }
+    catch (error) {
+      console.error('Error fetching access token:', error);
+    }
+
+  };
+
+
+  const fetchData = () => {
+    axios
+      .get("https://test.api.amadeus.com/v2/shopping/flight-offers", {
+        headers: {
+          Authorization: `Bearer ${accessT} `,
+        },
+        params: searchParams,
+      }).then((response) => {
+        const newData = response.data.data;
+        setData(newData);
+      }).
+      catch((error) =>
+        console.error("Error making API request:", error)
+      );
+  };
+
+  useEffect(() => {
+    if (!accessT || currentTimeInSeconds() > expiresIn) {
+      fetchAcessToken();
+      // console.log("Access Token:", accessToken);
+    }
+    else {
+      console.log("Token expires at ", new Date(expiresIn * 1000).toLocaleString());
+      if (!data) fetchData();
+    }
+  }, []);
+
   return (
     <>Hello world</>
   );
