423bec0a2fc362c06414c06aba86e457bf6f20c0
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2023-11-16 17:52:57 +0000

    add login/registration code.

--- FPP_client/src/App.jsx
+++ FPP_client/src/App.jsx
@@ -1,164 +1,69 @@
-import axios from "axios";
-import './App.css';
-import { useState, useEffect } from "react";
-import { List, ListItem, Card, CardContent, Typography, Box, Grid, CardMedia, AppBar, Toolbar, Button } from '@mui/material';
-import dayjs from 'dayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// src/App.js
+import React, { useState } from 'react';
 
 function App() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
 
-  const [data, setData] = useState(null);
+    const handleRegister = async () => {
+        try {
+            const response = await fetch('https://laughing-space-system-674p77v46p52r667-5000.app.github.dev/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
 
-  const apiKey = process.env.API_KEY;
-  const apiSecret = process.env.API_SECRET;
-
-  const accessT = localStorage.getItem("access_token");
-  const expiresIn = localStorage.getItem("expires_in");
-  const setCurrDate = () => {
-    const today = new Date();
-    const tomorrow = new Date(today);
-    tomorrow.setDate(today.getDate() + 1);
-    return tomorrow.toISOString().split("T")[0];
-
-  };
-  const [departDate, setDepartDate] = useState(dayjs(setCurrDate()));
-  console.log(departDate.format('YYYY-MM-DD'));
-  const searchParams = {
-    originLocationCode: "DEL",
-    destinationLocationCode: "BOM",
-    departureDate: departDate.format('YYYY-MM-DD'),
-    adults: 1,
-    currencyCode: "INR",
-    max: 20,
-  };
-
-  const currentTimeInSeconds = () => { return Math.floor(Date.now() / 1000); };
-
-
-  const fetchAcessToken = async () => {
-
-    const tokenRequestData = {
-      grant_type: "client_credentials",
-      client_id: apiKey,
-      client_secret: apiSecret,
+            const data = await response.json();
+            setMessage(data.message);
+        } catch (error) {
+            console.error('Error during registration:', error);
+        }
     };
 
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
+    const handleLogin = async () => {
+        try {
+            const response = await fetch('https://laughing-space-system-674p77v46p52r667-5000.app.github.dev/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
+
+            const data = await response.json();
+            setMessage(data.message);
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
     };
 
-    try {
-      const response = await axios
-        .post(
-          "https://test.api.amadeus.com/v1/security/oauth2/token",
-          tokenRequestData,
-          {
-            headers: headers,
-          }
-        );
-      localStorage.setItem("access_token", response.data.access_token);
-      localStorage.setItem("expires_in", currentTimeInSeconds() + response.data.expires_in);
-    }
-    catch (error) {
-      console.error('Error fetching access token:', error);
-    }
-
-  };
-
-
-  const fetchData = () => {
-    axios
-      .get("https://test.api.amadeus.com/v2/shopping/flight-offers", {
-        headers: {
-          Authorization: `Bearer ${accessT} `,
-        },
-        params: searchParams,
-      }).then((response) => {
-        const newData = response.data.data;
-        setData(newData);
-        console.log(newData);
-      }).
-      catch((error) =>
-        console.error("Error making API request:", error)
-      );
-  };
-
-  const fetchlogo = (item) => {
-    return item.itineraries[0]["segments"][0]["operating"]["carrierCode"];
-  };
-
-  const handlDateTime = (item, key) => {
-    const dateObj = new Date(item.itineraries[0]["segments"][0][key]["at"]);
-    const formattedDate = dateObj.toLocaleString("en-US", {
-      timeZone: "Asia/Kolkata",
-    });
-    return formattedDate;
-  };
-  useEffect(() => {
-    if (!accessT || currentTimeInSeconds() > expiresIn) {
-      fetchAcessToken();
-    }
-    else {
-      console.log("Token expires at ", new Date(expiresIn * 1000).toLocaleString());
-      fetchData();
-    }
-  }, [departDate]);
-
-  return (
-    <Box >
-      <AppBar position="sticky">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={ { flexGrow: 1 } }>
-            ✈️ Flight price prediction
-          </Typography>
-          <Button color="inherit">Login out</Button>
-        </Toolbar>
-      </AppBar>
-      <Box sx={ { marginTop: "2rem", marginBottom: "2rem", display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "center", width: 1 } }>
-        <LocalizationProvider dateAdapter={ AdapterDayjs }>
-          <DatePicker
-            label="Departure"
-            value={ departDate }
-            onChange={ (newValue) => setDepartDate(newValue) }
-          />
-        </LocalizationProvider>
-      </Box>
-      {
-        data != null ? (
-          <List >
-            { data.map((item, index) =>
-              <ListItem key={ item.id } >
-                <Grid container spacing={ 2 }>
-                  <Grid item sx={ { display: "flex", justifyContent: "center", alignItems: "center", width: 1 } }>
-                    <Card sx={ { width: '85%' } }>
-                      <CardContent sx={ { display: "flex", flexDirection: "row", justifyContent: "space-evenly", alignItems: "center" } }>
-                        <Typography sx={ { marginRight: "2rem" } }>{ item.id }</Typography>
-                        <CardMedia
-                          component="img"
-                          sx={ { width: "70px", height: "70px", objectFit: "contain", marginRight: "2rem" } }
-                          image={ `https://pics.avs.io/640/320/${fetchlogo(item)}.png` }
-                        ></CardMedia>
-                        <Typography sx={ { display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "10%", marginRight: "2rem" } }> { item.itineraries[0]["segments"][0]["departure"]["iataCode"] }<ArrowForwardIcon /> { item.itineraries[0]["segments"][0]["arrival"]["iataCode"] }</Typography>
-                        <Typography sx={ { width: "30%", marginRight: "1rem" } }>Departure: { handlDateTime(item, "departure") }</Typography>
-                        <Typography sx={ { width: "30%", marginRight: "1rem" } }>Arrival: { handlDateTime(item, "arrival") }</Typography>
-                        {/* <Typography>Carrier code: {item.itineraries[0]["segments"][0]["operating"]["carrierCode"]}</Typography> */ }
-                        <Typography sx={ { display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "5%", marginRight: "2rem" } }><CurrencyRupeeIcon />{ item["price"]["total"] }</Typography>
-
-                      </CardContent>
-
-                    </Card>
-                  </Grid>
-                </Grid>
-              </ListItem>) }
-          </List>
-        ) : (<>Hello world</>)
-      }
-    </Box >
-  );
+    return (
+        <div className="App">
+            <h1>Login and Register</h1>
+            <div>
+                <label>
+                    Username:
+                    <input type="text" value={ username } onChange={ (e) => setUsername(e.target.value) } />
+                </label>
+            </div>
+            <div>
+                <label>
+                    Password:
+                    <input type="password" value={ password } onChange={ (e) => setPassword(e.target.value) } />
+                </label>
+            </div>
+            <div>
+                <button onClick={ handleRegister }>Register</button>
+                <button onClick={ handleLogin }>Login</button>
+            </div>
+            <div>
+                <p>{ message }</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ FPP_server/app.py
@@ -0,0 +1,82 @@
+# app.py
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": "https://laughing-space-system-674p77v46p52r667-5173.app.github.dev"
+        }
+    },
+    supports_credentials=True,
+)
+api = Api(app)
+db_file_path = "sqlite:///users.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_file_path
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
+if not os.path.exists(db_file_path):
+    with app.app_context():
+        db.create_all()
+
+
+parser = reqparse.RequestParser()
+parser.add_argument("username", type=str, help="Username for registration")
+parser.add_argument("password", type=str, help="Password for registration and login")
+
+
+class HelloWorldResource(Resource):
+    def get(self):
+        return {"message": "Hello, World!"}
+
+
+class RegistrationResource(Resource):
+    def post(self):
+        args = parser.parse_args()
+        username = args["username"]
+        password = args["password"]
+
+        with app.app_context():
+            if User.query.filter_by(username=username).first():
+                return {"message": "User already exists"}, 400
+
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+
+        return {"message": "User registered successfully"}, 201
+
+
+class LoginResource(Resource):
+    def post(self):
+        args = parser.parse_args()
+        username = args["username"]
+        password = args["password"]
+
+        with app.app_context():
+            user = User.query.filter_by(username=username, password=password).first()
+
+        if user:
+            return {"message": "Login successful"}, 200
+        else:
+            return {"message": "Invalid credentials"}, 401
+
+
+api.add_resource(HelloWorldResource, "/")
+api.add_resource(RegistrationResource, "/register")
+api.add_resource(LoginResource, "/login")
+
+if __name__ == "__main__":
+    app.run(debug=True)
